478635799c37409a501af58868aee5be82fe9cea
dahBader <baderx98@gmail.com>
2019-12-02 21:35:32 +0100

    adding two partitions in the project

--- WpfGestionEtudiant/MainWindow.xaml.cs
+++ WpfGestionEtudiant/MainWindow.xaml.cs
@@ -24,5 +24,12 @@ namespace WpfGestionEtudiant
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            namelabel.Content = "bader";
+        }
+
+
     }
 }
---
+++ WpfGestionEtudiant/MainWindowPartMouad.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfGestionEtudiant
+{
+    public partial class MainWindow : Window
+    {
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            namelabel.Content = "mouad";
+        }
+
+    }
+}
